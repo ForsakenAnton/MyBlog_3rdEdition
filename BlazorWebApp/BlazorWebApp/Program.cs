e17045c4bb4683991bf002d422af35bc98a45765
Anton <forsakenanton@gmail.com>
2024-05-14 13:37:00 +0300

    Adding the API to Blazor

--- BlazorWebApp/BlazorWebApp/Program.cs
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -1,12 +1,28 @@
 using BlazorWebApp.Client.Pages;
 using BlazorWebApp.Components;
 
+using Data;
+using Data.Models.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services
+    .AddOptions<BlogApiJsonDirectAccessSetting>()
+    .Configure(options =>
+    {
+        options.DataPath = @"..\..\..\Data\";
+        options.BlogPostsFolder = "Blogposts";
+        options.TagsFolder = "Tags";
+        options.CategoriesFolder = "Categories";
+        options.CommentsFolder = "Comments";
+    });
+
+builder.Services.AddScoped<IBlogApi, BlogApiJsonDirectAccess>();
 
 var app = builder.Build();
 
